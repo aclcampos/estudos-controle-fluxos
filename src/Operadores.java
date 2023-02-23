feb12bce5615699a8b70dcca2ea1780deb5aab48
aclcampos <anaclaracamposbatista96@gmail.com>
2023-02-23 18:59:22 -0300

    Exemplos dos estudos feitos.

---
+++ src/Operadores.java
@@ -0,0 +1,45 @@
+public class Operadores {
+    /**
+     * Comparações operadores lógicos e relacionados, controle de fluxo e blocos.
+     */
+
+    public static void main(String[] args) throws Exception {
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'z';
+        String s1 = "Alguem";
+        String s2 = "Alguem2";
+        String s3 = "Fulano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1507L;
+        long l2 = 8907L;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("i1 == i2" + (i1 == i2));
+        System.out.println("i1 != i2" + (i1 != i2));
+        System.out.println("i1 > i2" + (i1 > i2));
+        System.out.println("i1<=i2" + (i1 <= i2));
+    }
+    /*
+     * Operadores Lógicos
+     * Conjunção=> Operação lógica que só é verdadeira quando ambos os operadores ou
+     * expressões envolvidas são verdade.
+     * Simbologia= &&
+     * Disjunção=> Operação que só é falsa quando ambos os operandos ou expressões
+     * forem falsos.
+     * Simbologia= ||
+     * Disjunção Exclusiva=> Operação lógica que só é verdadeira sem ambos os
+     * operandos ou expressões são opostos.
+     * Simbologia: ^
+     * Negação=> Operando que inverte o valor lógico de um operando ou expressão
+     * Simbologia= !
+     */
+
+}
